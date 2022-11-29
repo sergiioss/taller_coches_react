ea3d6451efcfef14d688690a094caf8dfa73d468
sergiioss <sergiiptx@gmail.com>
2022-11-29 01:28:35 +0100

    Background

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -9,7 +9,6 @@ const Button = () => {
             Button
         </div>
     )
-
 }
 
 export default Button
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import './Home.css'
 
-
 const Home = () => {
 
     return (
         <div className="home">
             <div>
                 <div className="sidebar">
-                    <div className="links one">Hola</div>
+                    <div><a className="links one" href="http://www.google.com" target="blank" >Hola</a></div>
                     <div className="links">Adios</div>
                     <div className="links">Hasta Luego</div>
                     <div className="links">Bien Hecho</div>
                     <div className="links">Mañana</div>
                 </div>
             </div>
+            <div className="cont">
+                <div className="news">
+                    HOLA
+                </div>
+            </div>
         </div>
     )
-
 }
 
 export default Home
