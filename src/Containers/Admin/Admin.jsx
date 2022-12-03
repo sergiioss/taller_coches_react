c4cfaa562e5420e401e494bf049d57c0b8c3bb85
sergiioss <sergiiptx@gmail.com>
2022-12-03 13:51:57 +0100

    Back arrow

--- src/Containers/Admin/Admin.jsx
+++ src/Containers/Admin/Admin.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Admin.css'
+import { NavLink } from 'react-router-dom'
 
 
 const Admin = () => {
 
     return (
         <div className="admin">
-            Admin
+            <div><NavLink className="back" to="/"><img className="arrow" src="https://cdn-icons-png.flaticon.com/512/1174/1174407.png"></img>Atras</NavLink>
+            </div>
         </div>
     )
 
--- src/Containers/Repairs/Repairs.jsx
+++ src/Containers/Repairs/Repairs.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Repairs.css'
+import { NavLink } from 'react-router-dom'
 
 
 const Repairs = () => {
 
     return (
         <div className="repairs">
-            Repairs
+            <div><NavLink className="back" to="/"><img className="arrow" src="https://cdn-icons-png.flaticon.com/512/1174/1174407.png"></img>Atras</NavLink>
+            </div>
         </div>
     )
 
