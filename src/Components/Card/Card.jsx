63786c5dd45791a83442daabaf3eb14f5e6634c6
sergiioss <sergiiptx@gmail.com>
2022-12-07 18:57:17 +0100

    Tab updated

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -8,13 +8,21 @@ const Card = (props) => {
             {/* ************** <-- Header --> ************* */}
 
             <div className="head">
-                <div className="headF"></div>
+                <div className="headF"><img className="logo" src="https://cdn.niviweb.com/images/coches/logo-taller-1.png"/></div>
                 <div className="headS">
-                    <div className="headSF"></div>
-                    <div className="headSS"></div>
+                    <div className="headSF">REPARACION Nº</div>
+                    <div className="headSS">{props.data.repair_number}</div>
                 </div>
-                <div className="headT"></div>
-                <div className="headT"></div>
+                <div className="headT">
+                    <div className="headTS">NOMBRE</div>
+                    <div className="headTS">DNI</div>
+                    <div className="headTS">EMAIL</div>
+                </div>
+                <div className="headT">
+                    <div className="headTS">{props.data.name_and_surname}</div>
+                    <div className="headTS">{props.data.clientDni}</div>
+                    <div className="headTS">{props.data.clientEmail}</div>
+                </div>
             </div>
             <div className="separation"></div>
             {/* ************** <-- Body --> ************* */}
@@ -25,42 +33,36 @@ const Card = (props) => {
                     <div className="bodContS">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi blanditiis et eligendi saepe voluptas. Error pariatur temporibus reprehenderit dolorum tenetur perferendis nam optio fuga voluptatum quaerat, nulla modi! Unde, at?</div>
                     <div className="bodContT">
                         <div className="title">MODELO</div>
-                        <div className="description">Descripcion1</div>
+                        <div className="description">{props.data.model}</div>
                     </div>
                     <div className="bodContT">
                         <div className="title">MARCA</div>
-                        <div className="description">Descripcion2</div>
+                        <div className="description">{props.data.brand}</div>
                     </div>
                     <div className="bodContT">
-                        <div className="title">SERIE</div>
-                        <div className="description">Descripcion3</div>
+                        <div className="title">COMPAÑIA DE SEGUROS</div>
+                        <div className="description">{props.data.insurance_company}</div>
                     </div>
                     <div className="bodContT">
-                        <div className="title">UBICACIÓN</div>
-                        <div className="description">Descripcion4</div>
+                        <div className="title">FECHA DE ENTRADA</div>
+                        <div className="description">{props.data.entry_date}</div>
                     </div>
                     <div className="bodContT">
-                        <div className="title">FECHA DE COMPRA</div>
-                        <div className="description">Descripcion5</div>
+                        <div className="title">PRECIO</div>
+                        <div className="description">{props.data.price + " €"}</div>
                     </div>
                     <div className="bodContT">
-                        <div className="title">CODIGO DE MAQUINARIA</div>
-                        <div className="description">Descripcion6</div>
-                    </div>
-                    <div className="bodContT">
-                        <div className="title">GARANTIA</div>
-                        <div className="description">Descripcion7</div>
+                        <div className="title">FECHA MATRICULACION</div>
+                        <div className="description">{props.data.registration_year}</div>
                     </div>
                 </div>
-                <div className="bodIm"></div>
+                <div className="bodIm">{props.data.image.data}</div>
             </div>
             {/* ************** <-- Footer  TECHNICAL & SUPPORT--> ************* */}
             <div className="separation"></div>
             <div className="tec">
-                <div className="tecn">TECNICO</div>
-                <div className="tecDesc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremque tempora dicta quasi aliquam corrupti magnam quam quas facilis, delectus molestiae soluta accusantium esse!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ea facere eos autem rerum nam sed ad animi voluptatum optio! Accusantium omnis doloremque, alias repellat iusto dolorum provident tempore consectetur!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremque tempora dicta quasi aliquam corrupti magnam quam quas facilis, delectus molestiae soluta accusantium esse!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ea facere eos autem rerum nam sed ad animi voluptatum optio! Accusantium omnis doloremque, alias repellat iusto dolorum provident tempore consectetur!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremque tempora dicta quasi aliquam corrupti magnam quam quas</div>
+                <div className="tecn">AVERIA</div>
+                <div className="tecDesc">{props.data.fault_description}</div>
             </div>
             <div className="separation"></div>
             <div className="tec">
