3682115b4ac99b05c03deae4b9d622abd55b1ddc
sergiioss <sergiiptx@gmail.com>
2022-12-07 21:38:07 +0100

    Boostrap card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,77 +1,80 @@
 import React from 'react'
 import './Card.css'
+import {Container, Col, Row} from 'react-bootstrap'
 
 
 const Card = (props) => {
     return (
-        <div className="card">
+        <Container className="cardCont">
             {/* ************** <-- Header --> ************* */}
 
-            <div className="head">
-                <div className="headF"><img className="logo" src="https://cdn.niviweb.com/images/coches/logo-taller-1.png"/></div>
-                <div className="headS">
-                    <div className="headSF">REPARACION Nº</div>
-                    <div className="headSS">{props.data.repair_number}</div>
-                </div>
-                <div className="headT">
-                    <div className="headTS">NOMBRE</div>
-                    <div className="headTS">DNI</div>
-                    <div className="headTS">EMAIL</div>
-                </div>
-                <div className="headT">
-                    <div className="headTS">{props.data.name_and_surname}</div>
-                    <div className="headTS">{props.data.clientDni}</div>
-                    <div className="headTS">{props.data.clientEmail}</div>
-                </div>
-            </div>
-            <div className="separation"></div>
+            <Row>
+                <Col className="logEme"><img className="logEm" src="https://cdn.niviweb.com/images/coches/logo-taller-1.png"/></Col>
+                <Col>
+                    <Row className="repT">REPARACION Nº</Row>
+                    <Row className="repC">{props.data.repair_number}</Row>
+                </Col>
+                <Col>
+                    <Row className="htitle">NOMBRE</Row>
+                    <Row className="htitle">CALLE</Row>
+                    <Row className="htitle">DNI</Row>
+                    <Row className="htitle">EMAIL</Row>
+                </Col>
+                <Col>
+                    <Row className="htitleC">{props.data.name_and_surname}</Row>
+                    <Row className="htitleC">{props.data.address}</Row>
+                    <Row className="htitleC">{props.data.clientDni}</Row>
+                    <Row className="htitleC">{props.data.clientEmail}</Row>
+                </Col>
+            </Row>
+            <Col></Col>
             {/* ************** <-- Body --> ************* */}
 
-            <div className="bod">
-                <div className="bodCont">
-                    <div className="bodContF">Descripción General del Equipo</div>
-                    <div className="bodContS">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi blanditiis et eligendi saepe voluptas. Error pariatur temporibus reprehenderit dolorum tenetur perferendis nam optio fuga voluptatum quaerat, nulla modi! Unde, at?</div>
-                    <div className="bodContT">
-                        <div className="title">MODELO</div>
-                        <div className="description">{props.data.model}</div>
-                    </div>
-                    <div className="bodContT">
-                        <div className="title">MARCA</div>
-                        <div className="description">{props.data.brand}</div>
-                    </div>
-                    <div className="bodContT">
-                        <div className="title">COMPAÑIA DE SEGUROS</div>
-                        <div className="description">{props.data.insurance_company}</div>
-                    </div>
-                    <div className="bodContT">
-                        <div className="title">FECHA DE ENTRADA</div>
-                        <div className="description">{props.data.entry_date}</div>
-                    </div>
-                    <div className="bodContT">
-                        <div className="title">PRECIO</div>
-                        <div className="description">{props.data.price + " €"}</div>
-                    </div>
-                    <div className="bodContT">
-                        <div className="title">FECHA MATRICULACION</div>
-                        <div className="description">{props.data.registration_year}</div>
-                    </div>
-                </div>
-                <div className="bodIm">{props.data.image.data}</div>
-            </div>
+            <Col>
+                <Col>
+                    <Col className="descri">Descripción General de la Averia</Col>
+                    <Col className="descri content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi blanditiis et eligendi saepe voluptas. Error pariatur temporibus reprehenderit dolorum tenetur perferendis nam optio fuga voluptatum quaerat, nulla modi! Unde, at?</Col>
+                    <Row>
+                        <Col className="descri content">MODELO</Col>
+                        <Col>{props.data.model}</Col>
+                    </Row>
+                    <Row>
+                        <Col className="content">MARCA</Col>
+                        <Col>{props.data.brand}</Col>
+                    </Row>
+                    <Row>
+                        <Col className="content">COMPAÑIA DE SEGUROS</Col>
+                        <Col>{props.data.insurance_company}</Col>
+                    </Row>
+                    <Row>
+                        <Col className="content">FECHA DE ENTRADA</Col>
+                        <Col>{props.data.entry_date}</Col>
+                    </Row>
+                    <Row>
+                        <Col className="content">PRECIO</Col>
+                        <Col>{props.data.price + " €"}</Col>
+                    </Row>
+                    <Row>
+                        <Col className="content">FECHA MATRICULACION</Col>
+                        <Col>{props.data.registration_year}</Col>
+                    </Row>
+                </Col>
+                {/* <Row>{props.data.image.data}</Row> */}
+            </Col>
             {/* ************** <-- Footer  TECHNICAL & SUPPORT--> ************* */}
-            <div className="separation"></div>
-            <div className="tec">
-                <div className="tecn">AVERIA</div>
-                <div className="tecDesc">{props.data.fault_description}</div>
-            </div>
-            <div className="separation"></div>
-            <div className="tec">
-                <div className="tecn">MANTENIMIENTO</div>
-                <div className="tecDesc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremque tempora dicta quasi aliquam corrupti magnam quam quas facilis, delectus molestiae soluta accusantium esse!
+            <Col></Col>
+            <Col>
+                <Col>AVERIA</Col>
+                <Col>{props.data.fault_description}</Col>
+            </Col>
+            <Col></Col>
+            <Col>
+                <Col>MANTENIMIENTO</Col>
+                <Col>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremque tempora dicta quasi aliquam corrupti magnam quam quas facilis, delectus molestiae soluta accusantium esse!
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ea facere eos autem rerum nam sed ad animi voluptatum optio! Accusantium omnis doloremque, alias repellat iusto dolorum provident tempore consectetur!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremque tempora dicta quasi aliquam corrupti magnam quam quas facilis, delectus molestiae soluta accusantium esse!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ea facere eos autem rerum nam sed ad animi voluptatum optio! Accusantium omnis doloremque, alias repellat iusto dolorum provident tempore consectetur!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremque tempora dicta quasi aliquam corrupti magnam quam quas</div>
-            </div>
-        </div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ea facere eos autem rerum nam sed ad animi voluptatum optio! Accusantium omnis doloremque, alias repellat iusto dolorum provident tempore consectetur!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremque tempora dicta quasi aliquam corrupti magnam quam quas</Col>
+            </Col>
+        </Container>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 /* import {Provider} from 'react-redux'; */
 /* import store from './App'; */
 
