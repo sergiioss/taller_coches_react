d712f51616c0adc596065c2e2dd7cdbaf60892ee
sergiioss <sergiiptx@gmail.com>
2022-12-21 19:41:19 +0100

    Boostrap

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -8,73 +8,74 @@ const Card = (props) => {
 
             {/* **************************************** <-- Header --> *************************************** */}
 
-            <Row>
-                <Col className="logEme"><img className="logEm" src="https://cdn.niviweb.com/images/coches/logo-taller-1.png" /></Col>
-                <Col>
+            <Row xs={3} sm={12} md={12} lg={12} xl={12} xxl={12}>
+                <Col xs={8} sm={8} md={8} lg={12} xl={12} xxl={12} className="logEme"><img className="logEm" src="https://cdn.niviweb.com/images/coches/logo-taller-1.png" /></Col>
+                <Col xs={4} sm={4} md={4} lg={4} xl={12} xxl={4}>
                     <Row className="repT">REPARACION Nº</Row>
                     <Row className="repC">{props.data.repair_number}</Row>
                 </Col>
-                <Col>
+                <Col xs={6} sm={6} md={6} lg={4} xl={12} xxl={4}>
                     <Row className="htitle">NOMBRE</Row>
                     <Row className="htitle">CALLE</Row>
                     <Row className="htitle">DNI</Row>
                     <Row className="htitle">EMAIL</Row>
                 </Col>
-                <Col>
+                <Col xs={6} sm={6} md={6} lg={4} xl={12} xxl={4}>
                     <Row className="htitleC">{props.data.name_and_surname}</Row>
                     <Row className="htitleC">{props.data.address}</Row>
                     <Row className="htitleC">{props.data.clientDni}</Row>
                     <Row className="htitleC">{props.data.clientEmail}</Row>
                 </Col>
             </Row>
-            <Row className="titlDesc">Descripción general de la avería por el cliente</Row>
-            <Row className="contDesc">- Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi blanditiis et eligendi saepe voluptas. Error pariatur temporibus reprehenderit dolorum tenetur perferendis nam optio fuga voluptatum quaerat, nulla modi! Unde, at?</Row>
+            <Row xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} className="titlDesc">Descripción general de la avería por el cliente</Row>
+            <Row xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} className="contDesc">- Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi blanditiis et eligendi saepe voluptas. Error pariatur temporibus reprehenderit dolorum tenetur perferendis nam optio fuga voluptatum quaerat, nulla modi! Unde, at?</Row>
 
             {/* **************************************** <-- Body --> *************************************** */}
 
-            <Row className="Conts">
-                <Col className="ContsCo">
+            <Row>
+                {/* <Col xs={6} sm={6} lg={6} xl={6} xxl={12}>
                     <Row>
-                        <Col className="longModel">MODELO</Col>
-                        <Col className="shortModel">{props.data.model}</Col>
+                        <Row xs={4} sm={8} md={8} lg={8} xl={8} xxl={12} className="longModel">MODELO</Row>
+                        <Row xs={8} sm={4} md={4} lg={4} xl={4} xxl={12} className="shortModel">{props.data.model}</Row>
                     </Row>
                     <Row>
-                        <Col className="longModel">MARCA</Col>
-                        <Col className="shortModel">{props.data.brand}</Col>
+                        <Row xs={10} sm={8} md={8} lg={8} xl={8} xxl={12} className="longModel">MARCA</Row>
+                        <Row xs={2} sm={4} md={4} lg={4} xl={4} xxl={12} className="shortModel">{props.data.brand}</Row>
                     </Row>
                     <Row>
-                        <Col className="longModel">COMPAÑIA DE SEGUROS</Col>
-                        <Col className="shortModel">{props.data.insurance_company}</Col>
+                        <Row xs={10} sm={8} md={8} lg={8} xl={8} xxl={12} className="longModel">COMPAÑIA DE SEGUROS</Row>
+                        <Row xs={2} sm={4} md={4} lg={4} xl={4} xxl={12} className="shortModel">{props.data.insurance_company}</Row>
                     </Row>
                     <Row>
-                        <Col className="longModel">FECHA DE ENTRADA</Col>
-                        <Col className="shortModel">{props.data.entry_date}</Col>
+                        <Row xs={10} sm={8} md={8} lg={8} xl={8} xxl={12} className="longModel">FECHA DE ENTRADA</Row>
+                        <Row xs={2} sm={4} md={4} lg={4} xl={4} xxl={12} className="shortModel">{props.data.entry_date}</Row>
                     </Row>
                     <Row>
-                        <Col className="longModel">PRECIO</Col>
-                        <Col className="shortModel">{props.data.price + " €"}</Col>
+                        <Row xs={10} sm={8} md={8} lg={8} xl={8} xxl={12} className="longModel">PRECIO</Row>
+                        <Row xs={2} sm={4} md={4} lg={4} xl={4} xxl={12} className="shortModel">{props.data.price + " €"}</Row>
                     </Row>
                     <Row>
-                        <Col className="longModel">FECHA DE MATRICULACION</Col>
-                        <Col className="shortModel">{props.data.registration_year}</Col>
+                        <Row xs={4} sm={8} md={8} lg={8} xl={8} xxl={12} className="longModel">FECHA DE MATRICULACION</Row>
+                        <Row xs={4} sm={4} md={4} lg={4} xl={4} xxl={12} className="shortModel">{props.data.registration_year}</Row>
                     </Row>
                 </Col>
-                <Col className="imgA">
-                    {props.data.image.type}
-                </Col>
+                <Col xs={6} sm={6} md={6} lg={6} xl={6} xxl={12} className="imgA">
+                        {props.data.image.type}
+                </Col> */}
             </Row>
 
             {/* **************************************** <-- Footer --> *************************************** */}
-
-            <Row className="titlDesc">AVERIA</Row>
-            <Row className="contDesc">
-                {"- " + props.data.fault_description}
-            </Row>
-            <Row className="titlDesc">MANTENIMIENTO</Row>
-            <Row className="contDesc">
-                - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ad error sed voluptas,   debitis alias doloremque tempora dicta quasi aliquam corrupti magnam quam quas facilis, delectus molestiae soluta    accusantium esse!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ea facere eos autem rerum nam sed ad animivoluptatum    optio! Accusantium omnis doloremque, alias repellat iusto dolorum provident tempore consectetur!Loremipsum dolor sit   amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremquetempora dicta quasi    aliquam corrupti magnam quam quas facilis, delectus molestiae soluta accusantium esse!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ea facere eos autem rerum nam sed ad animivoluptatum    optio! Accusantium omnis doloremque, alias repellat iusto dolorum provident tempore consectetur!Loremipsum dolor sit   amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremquetempora dicta quasi    aliquam corrupti magnam quam quas</Row>
+            <Col xs={12} sm={6} md={12} lg={12} xl={12} xxl={12}>
+                <Row xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} className="titlDesc">AVERIA</Row>
+                <Row xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} className="contDesc">
+                    {"- " + props.data.fault_description}
+                </Row>
+                <Row xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} className="titlDesc">MANTENIMIENTO</Row>
+                <Row xs={12} sm={12} md={12} lg={12} xl={12} xxl={12} className="contDesc">
+                    - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus ad error sed voluptas,   debitis alias doloremque tempora dicta quasi aliquam corrupti magnam quam quas facilis, delectus molestiae soluta    accusantium esse!
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ea facere eos autem rerum nam sed ad animivoluptatum    optio! Accusantium omnis doloremque, alias repellat iusto dolorum provident tempore consectetur!Loremipsum dolor sit   amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremquetempora dicta quasi    aliquam corrupti magnam quam quas facilis, delectus molestiae soluta accusantium esse!
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ea facere eos autem rerum nam sed ad animivoluptatum    optio! Accusantium omnis doloremque, alias repellat iusto dolorum provident tempore consectetur!Loremipsum dolor sit   amet, consectetur adipisicing elit. Ducimus ad error sed voluptas, debitis alias doloremquetempora dicta quasi    aliquam corrupti magnam quam quas</Row>
+            </Col>
         </Container>
     )
 }
